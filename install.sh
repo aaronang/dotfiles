b0b408dbaa24a550ef5f3cb9b70b224f0691d1b1
Aaron Ang <awz.ang@gmail.com>
2015-12-25 15:29:33 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -20,6 +20,11 @@ if ! command -v tmux > /dev/null 2>&1; then
     exit 1
 fi
 
+if ! command -v git > /dev/null 2>&1; then
+    echo "Tmux is not installed"
+    exit 1
+fi
+
 DOTFILES=$(pwd -P)
 
 echo "Creating symbolic links"
@@ -30,6 +35,8 @@ ln -s $DOTFILES/tmuxline ~/.tmuxline
 
 echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm ~/.zshrc
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
 echo "Installing NeoBundle"
 sh -c "$(curl -s https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh)"
