972ca974491603ef2fcf3ca085ddc5015a24b7de
Aaron Ang <awz.ang@gmail.com>
2016-11-24 15:55:32 +0100

    Remove unused scripts and configurations

--- bin/install.sh
+++ bin/install.sh
@@ -6,7 +6,7 @@ set -u
 read -r -p "Would you like to install the prerequisites for Ubuntu? [y/N]" response
 case $response in
     [yY][eE][sS]|[yY])
-        sudo apt-get install curl zsh git tmux vim-gnome
+        sudo apt-get install curl zsh git
         ;;
     *)
         ;;
@@ -18,22 +18,12 @@ mkdir -p ~/.config
 
 echo "Creating symbolic links"
 ln -s $DOTFILES/zshrc ~/.zshrc
-ln -s $DOTFILES/vimrc ~/.vimrc
-ln -s $DOTFILES/tmux.conf ~/.tmux.conf
-ln -s $DOTFILES/tmuxline ~/.tmuxline
 ln -s $DOTFILES/redshift.conf ~/.config/redshift.conf
-
-echo "Installing Nix"
-curl https://nixos.org/nix/install | sh
 
 echo "Changing default shell to Zsh"
 chsh -s $(which zsh) $(whoami)
 
 echo "Installing Oh My Zsh"
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm ~/.zshrc
 mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
-
-echo "Installing NeoBundle"
-sh -c "$(curl -s https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh)"
-vim +NeoBundleInstall +qall
--- zshrc
+++ zshrc
@@ -11,12 +11,3 @@ source $ZSH/oh-my-zsh.sh
 if [[ $TERMINIX_ID ]]; then
         source /etc/profile.d/vte.sh
 fi
-
-export GOPATH=/home/aaronang/Code/go
-export PATH=$PATH:$GOPATH/bin
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-export PATH="$PATH:$HOME/Applications/android-studio/bin"
-export PATH=~/.npm-global/bin:$PATH
-
